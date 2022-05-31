816729403fb2cc267dedf73da3628f0e463d0c73
Philip Rene Rothschild <philipr@egde.no>
2022-05-31 14:55:39 +0200

    Removing Chakra-UI project. Replaced by MUI

--- starstruck-nft-minting-app/src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID; 
-export const contractAddress = process.env.NEXT_PUBLIC_STARSTRUCK_NFT_CONTRACT ?? '';
-export const jsonRpcProvider = process.env.NEXT_PUBLIC_JSON_RPC_PROVIDER ?? '';
